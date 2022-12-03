b53f6cd9a259b3c36387891a0b894eb5afff6f92
mayaracsferreira <mayara_csf@hotmail.com>
2022-12-03 18:42:58 -0300

    [solved] Day 3 - Puzzle 2

--- day03/puzzles.py
+++ day03/puzzles.py
@@ -6,10 +6,12 @@
 ##  generate list of alphabet minuscule and maiuscule
 
 import string
+from itertools import islice
 
-INPUT_VALUES_FILE = 'input.txt'
+INPUT_VALUES_FILE = 'example-input.txt'
 ALPHABET_LIST = list(string.ascii_letters)
-priority_sum = 0
+puzzle1_priority = 0
+puzzle2_priority = 0
 
 def read_file(filename: str):
     file = open(filename)
@@ -20,22 +22,38 @@ def read_file(filename: str):
     finally:
         file.close()
 
-inputs = read_file(INPUT_VALUES_FILE).split()
-print(ALPHABET_LIST)
-for item in inputs:
+def spread_string_in_half(item: str):
     item_size = len(item)
     half = item_size//2
     first_half = set(item[:half])
     second_half = set(item[half:item_size])
-    common_character = list(first_half.intersection(second_half))[0]
-    priority_value = ALPHABET_LIST.index(common_character) + 1
-    priority_sum += priority_value    
-    print("Original string: {original}".format(original=item))
-    #print("First half {first_half}".format(first_half=first_half))
-    #print("Second half {second_half}".format(second_half=second_half))
-    print("Common value between two strings {common_character}".format(common_character=common_character))
-    print("Priority {priority}".format(priority=priority_value))
+    return first_half, second_half
+
+def find_common_character_between(item1: set, item2: set):
+    return list(item1.intersection(item2))[0]
+
+def find_common_characters(item):
     print()
 
-print(inputs)
-print("Priority sum {total}".format(total=priority_sum))
+def convert_into_priority(character: str ):
+    return ALPHABET_LIST.index(character) + 1
+
+def chunk(element, size):
+    it = iter(element)
+    return list(iter(lambda: tuple(islice(it, size)), ()))
+
+inputs = read_file(INPUT_VALUES_FILE).split()
+groups_of_three_list = chunk(inputs,3)
+
+for item in inputs:
+    half_parts = spread_string_in_half(item)
+    common_character = find_common_character_between(half_parts[0], half_parts[1])
+    priority_value = convert_into_priority(common_character)    
+    puzzle1_priority += priority_value       
+
+for item in groups_of_three_list:
+    common_value = list(set(item[0]) & set(item[1]) & set(item[2]))[0]
+    puzzle2_priority += convert_into_priority(common_value)
+
+print("Puzzle 1 - priority sum {total}".format(total=puzzle1_priority))
+print("Puzzle 2 - priority sum {total}".format(total=puzzle2_priority))
