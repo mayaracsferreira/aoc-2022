8b2b3bb56db52482dd00e3a0b063301371a77fac
mayaracsferreira <mayara_csf@hotmail.com>
2022-12-09 17:37:55 -0300

    [solved] Day 5 - Puzzle 2

--- day05/puzzles.py
+++ day05/puzzles.py
@@ -10,6 +10,7 @@
 
 import io
 import pandas as pd
+from copy import deepcopy
 
 INPUT_VALUES_FILE = 'input.txt'
 
@@ -22,10 +23,14 @@ def read_file(filename: str):
     finally:
         file.close()
 
+def get_last_characters(listname: list):
+    return [letter[-1] for letter in listname]
+
 # separate crates input from commands
 input_crates, input_commands = read_file(INPUT_VALUES_FILE).split("\n\n")
 commands = input_commands.split("\n")
 crates = input_crates.replace("    ", "[ ] ").replace("]", "],").replace("   ", ",")
+
 # commands: get how many cranes to move, intial position, final destination
 commands_list = []
 for command in commands:
@@ -49,12 +54,20 @@ for column in df.columns:
         crate.reverse()
         crates_list.append(crate)
 
+crates_list2 = deepcopy(crates_list)
+
 # apply commands to crane queues
-for command in commands_list:
+for index, command in enumerate(commands_list):
     crate_count, column_from, column_to = command
     pop_crates = [crates_list[column_from -1].pop() for idx in range(crate_count)]
     crates_list[column_to -1] += pop_crates
- 
-last_character_list = [letter[-1] for letter in crates_list]
-puzzle1_answer = ''.join(last_character_list)
+    # part two keep order
+    slice_crates = crates_list2[column_from-1][-crate_count:]
+    crates_list2[column_to -1] += slice_crates
+    del crates_list2[column_from -1 ][-crate_count:]    
+
+
+puzzle1_answer = ''.join(get_last_characters(crates_list))
+puzzle2_answer = ''.join(get_last_characters(crates_list2))
 print(puzzle1_answer)
+print(puzzle2_answer)
